f6adcb6c8990d438613714a5f57139d675097903
hannahrajarao <hannahrajarao@gmail.com>
2023-07-23 19:31:41 -0400

    Add prompt into function and functions to view tables

--- server.js
+++ server.js
@@ -2,20 +2,11 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// const db = mysql.createConnection(
-//     {
-//         host: 'localhost',
-//         user: process.env.DB_USER,
-//         password: process.env.DB_PASSWORD,
-//         database: 'employee_db'
-//     }
-// );
-
-inquirer.prompt([
-    {
-        type: 'list',
-
-        message: 'What would you like to do?',
+const inquireUser = async () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
             name: 'action',
             choices: [
                 'view all departments', 
@@ -25,10 +16,52 @@ inquirer.prompt([
                 'add a role', 
                 'add an employee', 
                 'update an employee role',
-            'quit'
-        ],
+                'quit',
+            ],
+        },
+    ])
+    .then((response) => {
+        // console.log(response);
+        const action = response.action;
+        if(action === 'quit') {
+            console.log("Bye!");
+            process.exit();
+        }
+        else if (action === 'view all departments'){
+            viewDepartments();
+        }
+        else if (action === 'view all roles') {
+            viewRoles();
+        }
+    });
+};
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: 'employee_db'
     },
-])
-.then(answers => {
-    console.log(answers)
-})
+    console.log("Connected to employee_db")
+);
+
+inquireUser();
+
+const viewDepartments = () => {
+    db.query('SELECT * FROM department', (err, results) => {
+        console.table(results);
+        return inquireUser();
+    });
+}
+
+const viewRoles = () => {
+    const sql = `
+        SELECT role.id, role.title, department.name AS department, role.salary 
+        FROM role 
+        JOIN department ON role.department_id = department.id;`
+    db.query(sql, (err, results) => {
+        console.table(results);
+        return inquireUser();
+    });
+}
