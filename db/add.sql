48fef50a8e3e7f87f558befde12a819699601d89
hannahrajarao <hannahrajarao@gmail.com>
2023-07-26 23:12:34 -0400

    Add role

--- db/add.sql
+++ db/add.sql
@@ -1,3 +1,7 @@
 -- Insert into department
 INSERT INTO department (name)
 VALUES ('Service')
+
+-- Insert into role
+INSERT INTO role (title, salary, department_id)
+VALUES ('Customer Service', 80000, 5)
