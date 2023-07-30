ae0f8ae5abd743d05f56fb3013e4db4f8bca5215
hannahrajarao <hannahrajarao@gmail.com>
2023-07-30 13:43:37 -0400

    Add employee

--- db/add.sql
+++ db/add.sql
@@ -1,7 +1,11 @@
 -- Insert into department
 INSERT INTO department (name)
-VALUES ('Service')
+VALUES ('Service');
 
 -- Insert into role
 INSERT INTO role (title, salary, department_id)
-VALUES ('Customer Service', 80000, 5)
+VALUES ('Customer Service', 80000, 5);
+
+-- Insert into employee
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Sam', 'Kash', 9, null);
